6ed788d952220fab7f808aec05da2ce990d45df0
Tim Schäfer <ts+code@rcmd.org>
2019-11-18 11:53:05 +0100

    write csv, disable test mode

--- src/run_geodesics.cpp
+++ src/run_geodesics.cpp
@@ -22,7 +22,7 @@ void run_geodesics(const int & nargs, const char ** args)
 	{
 		printf("./run_geodesics <inputfile> <outputfile> <method>\n");
 		printf("  <inputfile>  :  mesh file in format OFF or OBJ.\n");
-		printf("  <outputfile> :  output file location, will be in csv format.\n");
+		printf("  <outputfile> :  output file location, will be in text format.\n");
 		printf("  <method> :  1=ptp_cpu, 2=heatflow_cpu, 3=ptp_gpu, 4=heatflow_gpu.\n");
 
 		return;
@@ -31,6 +31,12 @@ void run_geodesics(const int & nargs, const char ** args)
 	const char * data_path = args[1];
 	const char * outputfile = args[2];
 	int method = atoi(args[3]);
+
+	int verbosity = 1;
+	int is_test = 0;		// If set, only 100 vertices are really computed. Used to timing different methods.
+	if(is_test == 1) {
+		printf("WARNING: Running in test mode, will only compute mean distance values for the first few vertices.\n");
+	}
 
 	cout << "Handling input file '" << data_path << "', using method " << method << "\n";
 	cout << "Will write to output file '" << outputfile << "'.\n";
@@ -42,7 +48,12 @@ void run_geodesics(const int & nargs, const char ** args)
 	cout << "Mesh with " << n_vertices << " vertices loaded.\n";
 	distance_t * mean_dists = new distance_t[mesh->n_vertices()];	// mean distance from one vertex to all others. kept over all iterations, one value added in each iteration.
 
-	for(index_t source_vert = 0; source_vert < 3; source_vert++) {
+	index_t verts_to_compute = n_vertices;
+	if(is_test == 1) {
+		verts_to_compute = 100;
+	}
+
+	for(index_t source_vert = 0; source_vert < verts_to_compute; source_vert++) {
 		vector <index_t> source = { source_vert };
 
 		index_t * toplesets = new index_t[n_vertices];
@@ -61,7 +72,7 @@ void run_geodesics(const int & nargs, const char ** args)
 
 
 		if(method == 1) {
-			cout << "Running ptp_cpu method\n";
+			//cout << "Running ptp_cpu method\n";
 			/* run_ptp_cpu(mesh, source, {limits, sorted_index}); */
 			dist = new distance_t[mesh->n_vertices()];	// geodesic distances of the source vertex to all other vertices. reset in each iteration.
 			const toplesets_t & toplesets2 = {limits, sorted_index};
@@ -111,25 +122,30 @@ void run_geodesics(const int & nargs, const char ** args)
 		delete [] sorted_index;
     if (dist) delete [] dist;
 
-		if(source_vert % 100 == 0) {
-		    printf("  At source vertex %d.\n", (int)source_vert);
-		}
-		else if (source_vert % 1000 == 0) {
-		    printf("At source vertex %d.\n", (int)source_vert);
-                }
+		if(verbosity > 0) {
+			if(source_vert % 100 == 0) {
+			    printf("  At source vertex %d.\n", (int)source_vert);
+			}
+			else if (source_vert % 1000 == 0) {
+			    printf("At source vertex %d.\n", (int)source_vert);
+	    }
+	  }
 
 
 	}
 
+	if(is_test == 1) {
+		printf("Mean geodesic distances for first 3 verts were: %f %f %f.\n", mean_dists[0], mean_dists[1], mean_dists[2]);
+	}
 	save_dists(outputfile, mean_dists, n_vertices);
-  printf("Saved mean distances to file '%s', exiting.\n", outputfile);
+  printf("Saved mean distances to file '%s'.\n", outputfile);
 
 }
 
 
 void save_dists(const char * outfile, distance_t * mean_dists, size_t num_dists) {
-	int method = 2;   // 1=binary, 2=CSV
-	if(method == 1) {
+	int save_method = 2;   // 1=binary, 2=CSV
+	if(save_method == 1) {
 		ofstream wf(outfile, ios::out | ios::binary);
 	   if(!wf) {
 	      cout << "Cannot open distance output file." << endl;
@@ -141,7 +157,7 @@ void save_dists(const char * outfile, distance_t * mean_dists, size_t num_dists)
 	      cout << "Error occurred while writing distances." << endl;
 	   }
  }
- if(method == 2) {
+ if(save_method == 2) {
 	 fstream f;
     f.open(outfile, ios::out);
     for(size_t i=0; i < num_dists; i++) {
